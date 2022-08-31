7808d40f7f00f918f3ce1045a7fcb6afd5a2d6e4
linkzzmbbsoffical <104633164+linkzzmbbsoffical@users.noreply.github.com>
2022-08-31 20:27:46 +0530

    Update gd_downloader.py

--- bot/helper/mirror_utils/download_utils/gd_downloader.py
+++ bot/helper/mirror_utils/download_utils/gd_downloader.py
@@ -8,7 +8,7 @@ from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMess
 from bot.helper.ext_utils.fs_utils import get_base_name
 
 
-def add_gd_download(link, path, listener, newname, is_gdtot):
+def add_gd_download(link, path, listener, newname):
     res, size, name, files = GoogleDriveHelper().helper(link)
     if res != "":
         return sendMessage(res, listener.bot, listener.message)
