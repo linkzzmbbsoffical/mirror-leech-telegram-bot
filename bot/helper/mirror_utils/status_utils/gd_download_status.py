d5e44ce5f99318028fc772dfa08c639f4fb54385
linkzzmbbsoffical <104633164+linkzzmbbsoffical@users.noreply.github.com>
2022-09-20 20:01:47 +0530

    Update gd_download_status.py

--- bot/helper/mirror_utils/status_utils/gd_download_status.py
+++ bot/helper/mirror_utils/status_utils/gd_download_status.py
@@ -1,4 +1,5 @@
 from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time
+from pkg_resources import get_distribution
 
 engine_ = f"Google Api v{get_distribution('google-api-python-client').version}"
 
